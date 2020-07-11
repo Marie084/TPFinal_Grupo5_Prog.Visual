c0e6adb26a25c543d04196c938456786dba0e49e
ANACHURI <ANACHURI@DESKTOP-MLIFJU9>
2020-07-10 22:52:59 -0300

    Descripcion:modifico WebSecurityConfig y header

--- src/main/java/ar/edu/unju/fi/WebSecurityConfig.java
+++ src/main/java/ar/edu/unju/fi/WebSecurityConfig.java
@@ -42,6 +42,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 			.logout()
 				.permitAll()
 				.logoutSuccessUrl("/login?logout");
+				 http.csrf().disable();
 	}
 	
 	BCryptPasswordEncoder bCryptPasswordEncoder;
